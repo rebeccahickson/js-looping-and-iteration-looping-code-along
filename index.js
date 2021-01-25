bb237fbd20d17c7871fb1890558f8a6f73ea2d46
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-01-25 14:07:18 -0500

    commit tree 54c62c4852f7fc96f952e0d90e7da0dadc3103cd

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(arr, event) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(
+      "Thank you, " + arr[i] + ", for the wonderful " + event + " gift!"
+    );
+  }
+  return newArr;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
